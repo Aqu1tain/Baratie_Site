34f2622a45bee90c0baacdfb3fa987085b1916d7
Aqu1tain <corentinfox08@gmail.com>
2024-02-05 11:39:03 +0100

    Homepage_achieved

--- src/components/burger.jsx
+++ src/components/burger.jsx
@@ -3,11 +3,41 @@ import React from "react";
 import "./local_styles/burger.scss";
 
 export default function Burger(props) {
+
+    function handleClick() {
+        // ADD active class
+        const burgerElement = document.querySelector(".burger");
+        let isActive = burgerElement.classList.contains("active");
+
+        if (burgerElement.classList.contains("active")) {
+            burgerElement.classList.remove("active");
+            document.querySelector(".overlayMenu").classList.remove("visible");
+
+            return;
+        } else {
+            burgerElement.classList.add("active");
+            document.querySelector(".overlayMenu").classList.add("visible");
+
+            return
+        }
+    }
+
     return (
-        <div className="burger" onClick={props.onClick} style={{...props.style}}>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
+        <>
+            <div className="burger" onClick={handleClick} style={{...props.style}}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className="overlayMenu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Menu</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </>
     )
 }
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,17 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { debounce } from "lodash";
 import "./local_styles/card.scss";
 
 export default function Card(props) {
-    return (
-        <div className="card">
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-            <ul>
-                {props.ingredients && props.ingredients.map((ingredient) => (
-                    <li>{ingredient}</li>
-                ))}
-            </ul>
-            <p>{props.children}</p>
-        </div>
-    )
+  return (
+    <div className="card">
+        <img
+        id={props.id}
+        className="card-img"
+        src={props.imgsrc}
+        alt=""
+        />
+        <h3>{props.title}</h3>
+        <p>{props.description}</p>
+        <ul>
+        {props.ingredients &&
+            props.ingredients.map((ingredient, index) => (
+            <li key={index}>{ingredient}</li>
+            ))}
+        </ul>
+        <p>{props.children}</p>
+    </div>
+    );
 }
